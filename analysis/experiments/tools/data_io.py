969d4d8a0da95a0a97643dce64998e00a84f8aa8
Michal Račko <michal.racko@threatmark.com>
2024-07-14 10:40:36 +0200

    General-question discriminator added

--- analysis/experiments/tools/data_io.py
+++ analysis/experiments/tools/data_io.py
@@ -1,8 +1,18 @@
+import random
+
 from pathlib import Path
 
 
-def read_user_questions(file_path: Path, n: int = None) -> list[str]:
+def read_user_questions(
+        file_path: Path,
+        n: int = None,
+        shuffle: bool = False,
+        random_seed: int = None,
+) -> list[str]:
     questions = file_path.read_text().strip().split('\n')
+    if shuffle:
+        random.seed(random_seed)
+        random.shuffle(questions)
     if n is None:
         return questions
     else:
